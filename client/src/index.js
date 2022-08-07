cfbf2b05966c2075623250b1e0edce402db404ee
Kate <k.shkurina365@gmail.com>
2022-08-08 07:21:14 +1300

    create mui theme

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {theme} from "./Styles/Theme";
+import {ThemeProvider} from "@mui/material";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+      <ThemeProvider theme={theme}>
+          <App />
+      </ThemeProvider>
   </React.StrictMode>
 );
 
