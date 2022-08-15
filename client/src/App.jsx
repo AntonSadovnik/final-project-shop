067cfc4d390fd364627ab52c0d7e1d8823d4903b
Godlevksyi Ruslan <rgodlevskyi@gmail.com>
2022-08-15 22:20:29 +0300

    plp layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,11 +3,13 @@ import './App.scss';
 // eslint-disable-next-line no-unused-vars
 import { Route, Routes } from 'react-router-dom';
 import Header from './components/Header/Header';
+import CardList from './components/CardList/CardList';
 
 function App() {
 	return (
 		<>
 			<Header />
+			<CardList/>
 			<Routes>{/* <Route path="/" element={<Main />}></Route> */}</Routes>
 			{/* <Footer /> */}
 		</>
