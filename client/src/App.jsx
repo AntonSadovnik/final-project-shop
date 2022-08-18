5307b529d645eaf3cd087b3f0c799e98055a1c2a
Larisa Doroshchuk <larisa.doroshchuk@gmail.com>
2022-08-18 15:29:04 +0300

    fixed conflicts

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,30 +1,31 @@
 import React from 'react';
 import './App.scss';
 import Grid from '@mui/material/Grid';
+import Header from './components/Header/Header';
+import Main from './components/Main/Main';
+import Nav from './components/Nav/Nav';
+import Footer from './components/Footer/Footer';
 import ProductCard from './components/Product_card/ProductCard';
 import Error from './components/Error/Error';
 
 function App() {
 	return (
-		<>
-			<Grid container justifyContent="center">
-				<Grid item xs={0} sm={2} md={2} lg={2} xl={2}
-				>
-					Nav
-				</Grid>
-
-				<Grid item xs={12} sm={9} md={9} lg={9} xl={9}>
-					<ProductCard />
-					<Error />
-				</Grid>
-
+		<Grid
+			container
+			justifyContent="center"
+			style={{ maxWidth: '2300px', margin: '0 auto' }}
+		>
+			<Grid item xs={2} sx={{ display: { xs: 'none', md: 'block' } }}>
+				<Nav />
 			</Grid>
-
-			{/* <Header /> */}
-			{/* <ProductCard /> */}
-			{/* <Routes><Route path="/" element={<Main />}></Route></Routes> */}
-			{/* <Footer /> */}
-		</>
+			<Grid item xl={9} md={10} xs={12}>
+				<Header />
+				<Main />
+				<ProductCard />
+				<Error />
+				<Footer />
+			</Grid>
+		</Grid>
 	);
 }
 
