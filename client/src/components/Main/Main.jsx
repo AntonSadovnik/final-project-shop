d3ade7b1b4e46291a3490e2ab6ca72bcd41b5d41
Concord <dmitry.concord@gmail.com>
2022-08-19 16:03:28 +0300

    sliders

--- client/src/components/Main/Main.jsx
+++ client/src/components/Main/Main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Grid from '@mui/material/Grid';
-import NoveltieSlider from '../Slider/comonents/noveltieSlider/Slider';
-import RecommendedSlider from '../Slider/comonents/recommendedSlider/SliderTwo';
+import NoveltieSlider from '../Slider/components/noveltieSlider/Slider';
+import RecommendedSlider from '../Slider/components/recommendedSlider/SliderTwo';
 
 function Main() {
 	return (
