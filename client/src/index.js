98d77c2f248cfc3bd1e278e69f0a9e410ba18a39
Kate <k.shkurina365@gmail.com>
2022-08-19 18:58:42 +0100

    after mergins conflicts

--- client/src/index.js
+++ client/src/index.js
@@ -5,18 +5,21 @@ import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { ThemeProvider } from '@mui/material';
+import { StyledEngineProvider } from '@mui/material/styles';
 import store from './store/store';
-import theme from './Styles/Theme';
+import theme from './styles/Theme';
 import App from './App';
 
 ReactDOM.render(
 	<React.StrictMode>
 		<ThemeProvider theme={theme}>
-			<BrowserRouter>
-				<Provider store={store}>
-					<App />
-				</Provider>
-			</BrowserRouter>
+			<StyledEngineProvider injectFirst>
+				<BrowserRouter>
+					<Provider store={store}>
+						<App />
+					</Provider>
+				</BrowserRouter>
+			</StyledEngineProvider>
 		</ThemeProvider>
 	</React.StrictMode>,
 	document.getElementById('root')
