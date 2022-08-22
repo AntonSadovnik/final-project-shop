e3b5751b2b8e8d70ef0307ab420ccacd5d4b0917
Vika Kramarenko <viktorikramar@gmail.com>
2022-08-22 18:07:44 +0200

    offer edits

--- client/src/components/Offer/Offer.jsx
+++ client/src/components/Offer/Offer.jsx
@@ -11,9 +11,14 @@ const OfferSlider = () => {
         const settings = {
             dots: true,
             infinite: true,
-            speed: 500,
+            speed: 1000,
             slidesToShow: 1,
-            slidesToScroll: 1
+            slidesToScroll: 1,
+            autoplay: true,
+            autoplaySpeed: 2000,
+            cssEase: "linear",
+            prevArrow: false,
+            nextArrow: false
         };
 
         return (
