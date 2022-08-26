56722e8a4cfa1b66dc76b0a567d7fdb5088366d4
Concord <dmitry.concord@gmail.com>
2022-08-26 13:20:13 +0300

    modify

--- client/src/components/Slider/components/buttons/NextBtn.jsx
+++ client/src/components/Slider/components/buttons/NextBtn.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import { Stack } from '@mui/material';
-import NavigateBeforeIcon from '@mui/icons-material/NavigateBefore';
+import NavigateNextIcon from '@mui/icons-material/NavigateNext';
+import PropTypes from 'prop-types';
 
 
 function NextBtn(props) {
   const { onClick } = props;
-  return (<Stack className='slider_btn' color="white" direction="row" justifyContent="center" alignItems="center" onClick={onClick} style={{background: "green", borderRadius: "50%", height:'60px', width:"60px",position:"absolute", top:"40%", left:"1%", zIndex:"1111"}}>
-    <NavigateBeforeIcon onClick={onClick}/>
-  </Stack>
+    return (<Stack className='slider_btn' color="white" direction="row" justifyContent="center" alignItems="center" onClick={onClick} style={{ background: "green", borderRadius: "50%", height:'60px', width:"60px",position:"absolute", top:"40%", right:"1%", zIndex:"1111" }}>
+    <NavigateNextIcon onClick={onclick} />
+    </Stack>
+    );
+  }
+  NextBtn.propTypes = {
+    onClick: PropTypes.func,
+  }
  
-  );
-}
-
-export default NextBtn
+  export default NextBtn
--- client/src/components/Slider/components/noveltieSlider/Slider.jsx
+++ client/src/components/Slider/components/noveltieSlider/Slider.jsx
@@ -1,16 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
 import Slider from 'react-slick';
-import { Stack } from '@mui/material';
+import { Stack, Button,Typography } from '@mui/material';
+import { getNoveltieProduct, getPopularProduct } from '../../../../api/Api';
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
 import "../../slider.scss";
-import productItems from "../../data";
 import NoveltiesCard from './NoveltiesCard';
-import NextBtn from '../buttons/NextBtn';
-import PrevBtn from '../buttons/PrevBtn';
+import NextBtn from '../buttons/PrevBtn';
+import PrevBtn from '../buttons/NextBtn';
+
 
 
 function NoveltieSlider() {
+const [items, setItems] = useState();
+const [button, setButton]= useState('left');
+
+ useEffect(()=>{
+  if(button==="left")
+  getNoveltieProduct().then(({data:{products}})=>setItems(products))
+  if(button==="right")
+  getPopularProduct().then(({data:{products}})=>setItems(products))
+ },[button])
+
   const settings = {
     dots: true,
     infinite: false,
@@ -55,16 +67,27 @@ function NoveltieSlider() {
       }
     ]
   };
-  
-	return ( <Stack className='slider-wrapper' direction="row" justifyContent="center" alignItems="center" sx={{marginTop: '50px'}}>
+  if(!items){return null}
+	return (<Stack direction="column" justifyContent="center" alignItems="center">
+  <Typography  component="div" className='btn-container' sx={{display:"flex", alignSelf:'flex-start'}}>
+ <Button onClick={()=>setButton('left')}  sx={{paddingLeft: 4, paddingRight:5, marginRight:'-10px' }}>Novelties</Button>
+ <Button  onClick={()=>setButton('right')} sx={{paddingLeft: 4, paddingRight:5, marginRight:'-10px' }}>Popular</Button>
+ </Typography>
+  <Stack className='slider-wrapper' direction="row" justifyContent="center" alignItems="center" sx={{marginTop: '50px'}}>
  
   <Slider {...settings}>
-  {productItems.map((item)=>(item.slider==="one") && <NoveltiesCard key={item.article} item={item}/>
-   
+
+  {items.map((item)=>  <NoveltiesCard key={item._id} item={item}/>
   )}
   
   </Slider>
+  </Stack>
   </Stack>)
+}
+
+NoveltieSlider.propTypes = {
+  name: PropTypes.string,
+  imageUrls: PropTypes.shape([]),
 }
 
 export default NoveltieSlider
