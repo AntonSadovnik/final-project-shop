5b96f23bcb69af2d78775c6f50682b7f30bf2ba9
Yelyzaveta Ahruts <agruts1997@gmail.com>
2022-08-30 17:28:54 +0300

    filter added

--- client/src/store/actions.js
+++ client/src/store/actions.js
@@ -1,5 +1,6 @@
 import getProducts from "../api/getProducts";
-import { ADD_TO_CART, ADD_FILTER} from "./types/types"
+import {getAllProducts} from "../api/Api";
+import {GET_PRODUCTS_INIT, ADD_TO_CART, ADD_FILTER} from "./types/types"
 
 export const getProductsAction = (categories) => (dispatch) => {
 	getProducts(categories).then((products) => {
@@ -7,6 +8,15 @@ export const getProductsAction = (categories) => (dispatch) => {
 		dispatch({ type: 'SET_PRODUCTS', payload: products.data });
 	});
 };
+
+export const getProductsRequest =()=> async(dispatch)=>{
+	try{
+		const {data} = await getAllProducts()
+		dispatch({type:GET_PRODUCTS_INIT,payload: data})
+	}catch(error){
+		console.log(error)
+	}
+}
 
 export const addToCart =(data)=> async(dispatch)=>{
 	try{
