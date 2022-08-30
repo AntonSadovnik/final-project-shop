824778b69e6d971435a42021681ccc9f78bd2a97
Yelyzaveta Ahruts <agruts1997@gmail.com>
2022-08-30 17:23:43 +0300

    filter added

--- client/src/store/actions.js
+++ client/src/store/actions.js
@@ -1,6 +1,5 @@
 import getProducts from "../api/getProducts";
-import {getAllProducts} from "../api/Api";
-import {GET_PRODUCTS_INIT, ADD_TO_CART} from "./types/types"
+import { ADD_TO_CART, ADD_FILTER} from "./types/types"
 
 export const getProductsAction = (categories) => (dispatch) => {
 	getProducts(categories).then((products) => {
@@ -8,16 +7,6 @@ export const getProductsAction = (categories) => (dispatch) => {
 		dispatch({ type: 'SET_PRODUCTS', payload: products.data });
 	});
 };
-
-
-export const getProductsRequest =()=> async(dispatch)=>{
-	try{
-		const {data} = await getAllProducts()
-		dispatch({type:GET_PRODUCTS_INIT,payload: data})
-	}catch(error){
-		console.log(error)
-	}
-}
 
 export const addToCart =(data)=> async(dispatch)=>{
 	try{
@@ -27,3 +16,11 @@ export const addToCart =(data)=> async(dispatch)=>{
 		console.log(error)
 	}
 }
+
+export const addFilter = (data) => (dispatch) => {
+	try {
+		dispatch({ type: ADD_FILTER, payload: data });
+	} catch (error) {
+		console.log(error);
+	}
+};
--- client/src/store/reducers/CartReducer.js
+++ client/src/store/reducers/CartReducer.js
@@ -1,15 +1,12 @@
-import {SET_PRODUCTS, ADD_TO_CART, DELETE_FROM_CART } from "../types/types";
+import { ADD_TO_CART, DELETE_FROM_CART } from "../types/types";
 
 const store ={cart:[],
 }
 
 const CartReducer = (state=store, action) => {
 	switch (action.type) {
-		case SET_PRODUCTS: {
-			return { ...state, products: action.payload };
-		}
 		case ADD_TO_CART: {
-		
+
 			return { ...state, cart: [...state.cart,action.payload] };
 		}
 		case DELETE_FROM_CART: {
