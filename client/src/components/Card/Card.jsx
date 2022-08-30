a025ddc0d3fa1bf304b0afb41b887a857724ec2f
Godlevksyi Ruslan <rgodlevskyi@gmail.com>
2022-08-30 19:40:09 +0300

    upgrade layout

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -1,59 +1,66 @@
 import React from 'react';
-import { Card,
+import {
+    Card,
     CardContent,
     CardMedia,
     Typography,
-    Button, 
+    Button,
     CardActions,
     Divider
- } from '@mui/material';
+} from '@mui/material';
 import PropTypes from 'prop-types';
 
 
-export default function ProductCard (props) {
-    const {data} = props;
-    const {title, portion, price, imgSrc, article} = data;
+export default function ProductCard(props) {
+    const { data } = props;
+    const { title, portion, price, imgSrc, article } = data;
     return (<Card key={article} className="card" sx={{
         borderRadius: 5,
         background: 'white',
         overflow: 'hidden',
-        maxWidth: '288px',
-        maxHeight: '400px',
         boxSizing: 'border-box',
-        padding: '0px',
-        margin: '0px'
-        }}>
-       <CardMedia
-              component="img"
-              alt="not display"
-              height="210px"
-              width="253px"
-              fit="cover"
-              image={imgSrc}
-              className="img"
-            />
-             <CardContent sx={{ paddingBottom: '0px' }}>
-              <Typography gutterBottom variant="h6" component="p" className='title' sx={{ fontSize: 22 }}>
-              {title}
-              </Typography>
-              <Typography variant="body2" component="p" color="text.secondary" className='portion' sx={{marginBottom: "0"}}>
-              {portion}
-              <Divider style={{color: 'black', marginTop: '20px'}} />
-              </Typography>
+        // maxWidth: {sm: 330}
+    }}>
+        <CardMedia 
+            component="img"
+            alt="not display"
+            sx={{
+                height: 210,
+                width: 253,
+                fit:"cover",
+                maxWith: {xs: 100, sm: 180, md: 253},
+                margin: 'auto'
+            }}
             
-              <Typography variant="body2" component="p"  className='card-bottom' sx={{position: "relative",
-              display: 'flex',justifyContent: 'space-between',padding: 0,}}>
-              <Typography variant="body2" component="p"  className='price' sx={{alignSelf: "center",
-        fontWeight: 700,
+            image={imgSrc}
+            className="img"
+        />
+        <CardContent sx={{ paddingBottom: '0px' }}>
+            <Typography gutterBottom variant="h6" component="p" className='title' sx={{ fontSize: '24px', fontWeight: 500 }}>
+                {title}
+            </Typography>
+            <Typography variant="body2" component="p" color="text.secondary" className='portion' sx={{ marginBottom: "0", fontSize: '18px', fontWeight: 400 }}>
+                {portion}
+                <Divider style={{ color: 'black', marginTop: '20px' }} />
+            </Typography>
+
+            <Typography variant="body2" component="p" className='card-bottom' sx={{
+                position: "relative",
+                display: 'flex', justifyContent: 'space-between', padding: 0,
+            }}>
+                <Typography variant="body2" component="p" sx={{
+                    alignSelf: "center",
+                    fontWeight: 700,
+                    fontSize: '24px',
                 }}>
-              {price}
-              </Typography>
-              <CardActions component="div">
-              <Button variant="contained" sx={{paddingLeft: 5, paddingRight:5 }}>Wish</Button>
-              </CardActions>    
-              </Typography>
-              </CardContent>
-          </Card>)
+                    {price}
+                </Typography>
+                <CardActions component="div">
+                    <Button variant="contained" sx={{ paddingLeft: 5, paddingRight: 5 }}>Wish</Button>
+                </CardActions>
+            </Typography>
+        </CardContent>
+    </Card>)
 }
 
 ProductCard.propTypes = {
--- client/src/components/CardList/CardList.jsx
+++ client/src/components/CardList/CardList.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import Grid from '@mui/material/Unstable_Grid2';
-import Paper from '@mui/material/Paper';
 import Stack from '@mui/material/Stack';
-import { styled } from '@mui/material/styles';
 import { Box, Typography } from '@mui/material';
 import ProductCard from '../Card/Card';
 import SortSelect from '../SortSelect/SortSelect';
@@ -13,35 +11,34 @@ import './cardList.scss'
 export default function CardList() {
 
     const listItems = items.map((item) =>
-        <Grid xs={4} key={item.id}>
+        <Grid xs={2} key={item.id}>
             <ProductCard data={item} />
         </Grid>
     );
 
-    const Item = styled(Paper)(({ theme }) => ({
-        backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-        ...theme.typography.body2,
-        padding: theme.spacing(1),
-        textAlign: 'center',
-        color: theme.palette.text.secondary,
-    }));
-
     return (
-        <Box align='center'>
-            <Stack direction="row" justifyContent="space-between" spacing={2}>
-                <Box flexDirection='row'>
-                    <img src='https://res.cloudinary.com/dh6o5rpfe/image/upload/v1660240222/sushi-shop/Nav/%D0%A1%D0%B5%D1%82%D1%8B_kqi7dj.svg' alt='' />
-                    <Typography component="h1" sx={{ fontSize: '20px', fontWeight: 'bold',}}>
+        <Box sx={{ padding: '30px 0px' }}>
+            <Stack direction="row" justifyContent="space-between" spacing={2} sx={{ paddingBottom: '30px' }}
+            xs={{ flexDirection: 'column' }}>
+                <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center', }} xs={{ flexDirection: 'column' }}>
+                    <Box
+                        component='img'
+                        sx={{
+                            height: 43
+                        }}
+                        alt='Sets svg'
+                        src='https://res.cloudinary.com/dh6o5rpfe/image/upload/v1660240222/sushi-shop/Nav/%D0%A1%D0%B5%D1%82%D1%8B_kqi7dj.svg'
+                    />
+                    {/* <img src='https://res.cloudinary.com/dh6o5rpfe/image/upload/v1660240222/sushi-shop/Nav/%D0%A1%D0%B5%D1%82%D1%8B_kqi7dj.svg' alt='' /> */}
+                    <Typography component="h1" sx={{ fontSize: '36px', fontWeight: 'bold', marginLeft: '20px'}}>
                         Sets
                     </Typography>
                 </Box>
                     <SortSelect className="select" />
 
             </Stack>
-            <Grid container spacing={6} sx={{
-                maxWidth: '1260px',
-                marginLeft: 'auto',
-                marginRight: 'auto',
+            <Grid container columns={{ xs: 2, md: 4, lg: 6 }} rowSpacing='30px' columnSpacing={{ md: '50px', lg: '40px' }} sx={{
+                maxWidth: '950px',
 
             }}>
                 {listItems}
