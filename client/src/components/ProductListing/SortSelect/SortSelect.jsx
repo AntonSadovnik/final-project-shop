b8fd1605e1ad0f8da3f752fa23f71d7ef065cad5
Anton Sadovnik <222anton02@gmail.com>
2022-09-07 18:53:33 +0200

    plp render and logic

--- client/src/components/ProductListing/SortSelect/SortSelect.jsx
+++ client/src/components/ProductListing/SortSelect/SortSelect.jsx
@@ -6,22 +6,19 @@ import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
 
 export default function SortSelect() {
-  return (
-    <Box sx={{ minWidth: 120 }}>
-      <FormControl fullWidth>
-        <InputLabel id="select-label">Sorting</InputLabel>
-        <Select
-          labelId="select-label"
-          label="Sorting"
-        >
-          <MenuItem value={5}>Default</MenuItem>
-          <MenuItem value={10}>Name</MenuItem>
-          <MenuItem value={20}>First cheaper</MenuItem>
-          <MenuItem value={30}>First more expensive</MenuItem>
-          <MenuItem value={40}>Number of pieces</MenuItem>
-          <MenuItem value={50}>The weight</MenuItem>
-        </Select>
-      </FormControl>
-    </Box>
-  );
+	return (
+		<Box sx={{ minWidth: 120 }}>
+			<FormControl fullWidth>
+				<InputLabel id="select-label">Sorting</InputLabel>
+				<Select labelId="select-label" label="Sorting">
+					<MenuItem value={5}>Default</MenuItem>
+					<MenuItem value={10}>Name</MenuItem>
+					<MenuItem value={20}>First cheaper</MenuItem>
+					<MenuItem value={30}>First more expensive</MenuItem>
+					<MenuItem value={40}>Number of pieces</MenuItem>
+					<MenuItem value={50}>The weight</MenuItem>
+				</Select>
+			</FormControl>
+		</Box>
+	);
 }
--- client/src/pages/Products/Products.jsx
+++ client/src/pages/Products/Products.jsx
@@ -1,21 +1,53 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from 'react';
+import { Box, Grid, Typography } from '@mui/material';
 import { useSearchParams } from 'react-router-dom';
-import {useDispatch, useSelector} from "react-redux";
-import {addToCart, getProductsAction} from "../../store/actions";
+import { useDispatch, useSelector } from 'react-redux';
+import { addToCart, getProductsAction } from '../../store/actions';
+import ProductCard from '../../components/ProductListing/Card/Card';
+import menuItemsContent from '../../components/Nav/config';
 
 function Products() {
-    const dispatch = useDispatch()
-    const [searchParams] = useSearchParams();
-    const currentParams = Object.fromEntries(searchParams);
-    console.log('currentParams.categories',currentParams.categories)
-    useEffect(() => {
-        dispatch(getProductsAction(`categories=${currentParams.categories}`))
-    }, [currentParams.categories])
-    const {products} = useSelector(state => state.products)
-    const components = products.map(product=><button onClick={()=>dispatch(addToCart(product))} type='button' key={product.itemNo}>{product.itemNo}</button>)
-        console.log(products)
-
-    return <main>Products page {components}</main>;
+	const dispatch = useDispatch();
+	const [searchParams] = useSearchParams();
+	const currentParams = Object.fromEntries(searchParams);
+	useEffect(() => {
+		dispatch(getProductsAction(`categories=${currentParams.categories}`));
+	}, [currentParams.categories]);
+	const { products } = useSelector((state) => state.products);
+	const components = products.map((product) => (
+		<ProductCard data={product} onClick={() => dispatch(addToCart(product))} />
+	));
+	const categoryTitle =
+		currentParams.categories.charAt(0).toUpperCase() +
+		currentParams.categories.slice(1);
+	const categoryImgPath = menuItemsContent().find(
+		(el) => el.alt.toLocaleLowerCase() === categoryTitle.toLocaleLowerCase()
+	).src;
+	return (
+		<main>
+			<Grid sx={{ padding: { xs: '15px 0 0', lg: '30px 111px 60px' } }}>
+				<Grid
+					container
+					sx={{
+						marginBottom: '30px',
+						justifyContent: { xs: 'center', lg: 'flex-start' },
+					}}
+				>
+					<Box
+						component="img"
+						src={categoryImgPath}
+						sx={{ marginRight: '20px' }}
+					/>
+					<Typography variant="h4" component="h2">
+						{categoryTitle}
+					</Typography>
+				</Grid>
+				<Grid container sx={{ rowGap: '50px' }}>
+					{components}
+				</Grid>
+			</Grid>
+		</main>
+	);
 }
 
 export default Products;
