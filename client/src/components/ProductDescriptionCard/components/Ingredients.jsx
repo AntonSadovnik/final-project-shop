3d04f82149456a8ed81aba37b8d62d66aeca034b
Anton Sadovnik <222anton02@gmail.com>
2022-09-15 15:59:16 +0200

    fixed ingredients

--- client/src/components/ProductDescriptionCard/components/Ingredients.jsx
+++ client/src/components/ProductDescriptionCard/components/Ingredients.jsx
@@ -4,39 +4,41 @@ import { Typography } from '@mui/material';
 function Ingredients(props) {
 	const { ingredients } = props;
 	return (
-		<>
-			<Typography
-				variant="h6"
-				component="h6"
-				sm={{
-					fontWeight: '500',
-					fontSize: '18px',
-					lineHeight: '22px',
-					margin: '26px 0 5px 0',
-				}}
-				sx={{
-					margin: { xs: '0 0 5px 0', md: '26px 0 5px 0' },
-					textAlign: { xs: 'center', md: 'start' },
-					color: { xs: '#111111' },
-				}}
-			>
-				Compound:
-			</Typography>
+		ingredients && (
+			<>
+				<Typography
+					variant="h6"
+					component="h6"
+					sm={{
+						fontWeight: '500',
+						fontSize: '18px',
+						lineHeight: '22px',
+						margin: '26px 0 5px 0',
+					}}
+					sx={{
+						margin: { xs: '0 0 5px 0', md: '26px 0 5px 0' },
+						textAlign: { xs: 'center', md: 'start' },
+						color: { xs: '#111111' },
+					}}
+				>
+					Compound:
+				</Typography>
 
-			<Typography
-				variant="body2"
-				component="p"
-				sx={{
-					fontWeight: '300',
-					fontSize: '18px',
-					lineHeight: '22px',
-					textAlign: { xs: 'center', md: 'start' },
-					color: { xs: '#111111' },
-				}}
-			>
-				{ingredients}
-			</Typography>
-		</>
+				<Typography
+					variant="body2"
+					component="p"
+					sx={{
+						fontWeight: '300',
+						fontSize: '18px',
+						lineHeight: '22px',
+						textAlign: { xs: 'center', md: 'start' },
+						color: { xs: '#111111' },
+					}}
+				>
+					{ingredients}
+				</Typography>
+			</>
+		)
 	);
 }
 
