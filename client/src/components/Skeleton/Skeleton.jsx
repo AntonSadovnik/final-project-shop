67a7ca457721f20d7254f34c835992e5d3407b96
Concord <dmitry.concord@gmail.com>
2022-09-15 23:22:52 +0300

    mod

--- client/src/components/Skeleton/Skeleton.jsx
+++ client/src/components/Skeleton/Skeleton.jsx
@@ -21,7 +21,7 @@ function OfferSliderSkeleton() {
 
    if(!items){
     content = <Box direction="column" justifyContent="center" alignItems="flex-end" className='skeleton' width='100%' height={540} sx={{bgcolor:'#F2F2F2', marginBottom: '100px', display:'flex', alignItems:"flex-end",padding:'0 5px'  }}>
-    <Stack className='slider-wrapper' direction="row" justifyContent="center" alignItems="center" maxWidth="940px" width="76vw" height={475} sx={{ bgcolor:'grey.400', borderRadius:'20px',paddingTop:'10px' }}>
+    <Stack className='slider-wrapper' direction="row" justifyContent="center" alignItems="center" maxWidth="950px" width="81vw" height={475} sx={{ bgcolor:'grey.400', borderRadius:'20px',paddingTop:'10px' }}>
     <Skeleton variant="rectangular" sx={{bgcolor:'grey.600'}} width='450px' height='250px' />
   
     <Typography direction="column" justifycontent="space-between" alignitems="flex-end" sx={{width:'45%', height:'100%', display:'flex', flexDirection:"column", alignItems:'flex-end',justifyContent:"space-around" }}>
