4a56ae8397edc57944ca50e17c304c8574e88b48
Anton Sadovnik <222anton02@gmail.com>
2022-09-25 16:29:35 +0200

    suggested changes

--- client/src/components/ProductDescriptionCard/ProductCard.jsx
+++ client/src/components/ProductDescriptionCard/ProductCard.jsx
@@ -1,6 +1,6 @@
 import { React, useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import { Stack, } from '@mui/material';
+import { Stack } from '@mui/material';
 import { useParams } from 'react-router-dom';
 import { getProduct, getProductsByCategory } from '../../api/Api';
 import Error from '../Error/Error';
@@ -17,36 +17,39 @@ function ProductCard() {
 
 	const dispatch = useDispatch();
 	const { id } = useParams();
-	const cardPicture = 'imageUrls' in product ? product.imageUrls[0] : '';
-	const cardWeight = 'weight' in product ? product.weight : '';
+	// const cardPicture = 'imageUrls' in product ? product.imageUrls[0] : '';
+	// const cardWeight = 'weight' in product ? product.weight : '';
 	const cardTitle = 'name' in product ? product.name : '';
-	const cardPrice = 'currentPrice' in product ? product.currentPrice : '';
-	const cardPpromoPrice = 'previousPrice' in product ? product.previousPrice : '';
-	let cardCompound = 'ingredients' in product ? product.ingredients : '';
-	cardCompound = 'contains' in product ? product.contains.toString() : cardCompound;
-	const displayCompound = cardCompound !== '' ? 'block' : 'none';
-	const displaycardPpromoPrice = cardPpromoPrice !== '' ? 'block' : 'none';
-	const grams = product.categories === 'drinks' ? 'ml' : 'grams';
+	// const cardPrice = 'currentPrice' in product ? product.currentPrice : '';
+	// const cardPpromoPrice = 'previousPrice' in product ? product.previousPrice : '';
+	// let cardCompound = 'ingredients' in product ? product.ingredients : '';
+	// cardCompound = 'contains' in product ? product.contains.toString() : cardCompound;
+	// const displayCompound = cardCompound !== '' ? 'block' : 'none';
+	// const displaycardPpromoPrice = cardPpromoPrice !== '' ? 'block' : 'none';
+	// const grams = product.categories === 'drinks' ? 'ml' : 'grams';
 	const categorySlider = product.categories === 'drinks' ? 'sushi' : 'drinks';
 
 	async function showProduct() {
-
 		await getProduct(id).then(({ data: { products } }) => {
-			if (products.length === 0) return setProduct({ "error": 404 });
+			if (products.length === 0) return setProduct({ error: 404 });
 			getProductsByCategory(products[0].categories).then((data) => {
 				const currentProductId = data.data.products.findIndex(
 					(element) => element.itemNo === id
 				);
 
-				const forvardProd = typeof data.data.products[currentProductId + 1] === 'undefined'
-					? 0 : currentProductId + 1;
+				const forvardProd =
+					typeof data.data.products[currentProductId + 1] === 'undefined'
+						? 0
+						: currentProductId + 1;
 
 				setForwardProductId(
 					`/products/${data.data.products[forvardProd].itemNo}`
 				);
 
-				const backProd = typeof data.data.products[currentProductId - 1] === 'undefined'
-					? data.data.products.length - 1 : currentProductId - 1;
+				const backProd =
+					typeof data.data.products[currentProductId - 1] === 'undefined'
+						? data.data.products.length - 1
+						: currentProductId - 1;
 
 				setBackProductId(`/products/${data.data.products[backProd].itemNo}`);
 			});
@@ -54,17 +57,23 @@ function ProductCard() {
 		});
 	}
 
-	useEffect(() => { showProduct(); }, []);
+	useEffect(() => {
+		showProduct();
+	}, []);
 
 	useEffect(() => {
 		showProduct();
 		setQuantityGoods(1);
 	}, [id]);
 
-	const addQuantity = () => { setQuantityGoods(quantityGoods + 1); };
+	const addQuantity = () => {
+		setQuantityGoods(quantityGoods + 1);
+	};
 
 	const minusQuantity = () => {
-		if (quantityGoods > 1) { setQuantityGoods(quantityGoods - 1); }
+		if (quantityGoods > 1) {
+			setQuantityGoods(quantityGoods - 1);
+		}
 	};
 
 	const onClickButton = () => {
@@ -72,25 +81,30 @@ function ProductCard() {
 		dispatch(addToCart(product));
 	};
 
-	if (cardTitle !== "") {
+	if (cardTitle !== '') {
 		return (
 			<>
-				<Stack style={{ margin: '0 auto', }} >
+				<Stack style={{ margin: '0 auto' }}>
+					<BlockSwitchingInCategory
+						backProductId={backProductId}
+						forwardProductId={forwardProductId}
+					/>
 
-					<BlockSwitchingInCategory backProductId={backProductId} forwardProductId={forwardProductId} />
-
-					<BlockDrawCard cardPicture={cardPicture} cardTitle={cardTitle} cardWeight={cardWeight} grams={grams}
-						displaycardPpromoPrice={displaycardPpromoPrice} cardPpromoPrice={cardPpromoPrice}
-						cardPrice={cardPrice} minusQuantity={minusQuantity} quantityGoods={quantityGoods} addQuantity={addQuantity}
-						displayCompound={displayCompound} cardCompound={cardCompound} onClickButton={onClickButton}
+					<BlockDrawCard
+						product={product}
+						minusQuantity={minusQuantity}
+						quantityGoods={quantityGoods}
+						addQuantity={addQuantity}
+						onClickButton={onClickButton}
 					/>
 				</Stack>
 
 				<RecommendedSlider category={categorySlider} />
 			</>
-		)
+		);
 	}
 	if (product.error === 404) {
-		return (<Error />)
+		return <Error />;
 	}
-} export default ProductCard;
+}
+export default ProductCard;
